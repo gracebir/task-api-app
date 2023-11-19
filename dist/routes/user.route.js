3a2fd2bad0431c920617770669536d808171511b
gracebir <grace.devolop@gmail.com>
2023-11-19 13:39:20 +0200

    login route

--- dist/routes/user.route.js
+++ dist/routes/user.route.js
@@ -5,5 +5,5 @@ const register_1 = require("../controllers/register");
 const authentication_1 = require("../middleware/authentication");
 const router = (0, express_1.Router)();
 router.post('/signup', register_1.register);
-router.post('/signin', authentication_1.signin);
+router.post('/', authentication_1.signin);
 exports.default = router;
